08856fcb6e6e45fe525e167776c0c6792fd2df56
Johnny Phan <pjohnny550@gmail.com>
2023-01-08 15:55:44 -0800

    final game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,15 +1,55 @@
 import random
+#test
 
-choices = ["rock","paper", "scissor"]
-def AI_choice():
+
+while True:
+    choices = ["rock", "paper", "scissor"]
     result = random.choice(choices)
-    print(result)
 
-answer = input("Would you like to play a game?: (yes/no)")
-if answer.lower() == "yes":
-    game = input("rock,paper,scissor?: ")
-else:
-    print("Bye")
-    quit()
+
+    answer = input("Would you like to play?: (yes/no)")
+    if answer.lower() == "yes":
+        person_choice = input("rock,paper,scissor?: ").lower()
+    else:
+        print("BYE!")
+
+        quit()
+
+    if person_choice == result:
+        print(f"You:{person_choice}")
+        print(f"Computer:{result}")
+        print("[TIE!]")
+
+    elif person_choice == "rock" and result == "scissor":
+        print(f"You:{person_choice}")
+        print(f"Computer:{result}")
+        print("[You win]")
+    elif person_choice == "scissor" and result == "paper":
+        print(f"You:{person_choice}")
+        print(f"Computer:{result}")
+        print("[You win]")
+    elif person_choice == "paper" and result == "rock":
+        print(f"You:{person_choice}")
+        print(f"Computer:{result}")
+        print("[You win]")
+    else:
+        print(f"You:{person_choice}")
+        print(f"Computer:{result}")
+        print("[You lose]")
+    continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
